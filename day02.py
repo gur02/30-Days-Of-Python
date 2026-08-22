435e16f2aafce1262f39557255ec13c3a34172c3
gur02 <gur02bains@gmail.com>
2026-08-22 18:18:21 +0530

    Create day02.py

---
+++ day02.py
@@ -0,0 +1,8 @@
+student_name="Gurjit Kaur"
+current_day=2
+daily_hours=1.5
+aiming_for_pro=True
+print("Student name:", student_name)
+print("clallenge Day:", current_day)
+print("Data Type of student_name:", type(student_name))
+print("Data Type of cureent_day:",type(current_day))
