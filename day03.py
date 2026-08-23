0a1c61a54e00548d86999b1b53787b5db0caf0a5
gur02 <gur02bains@gmail.com>
2026-08-23 13:58:16 +0530

    Create day03.py

---
+++ day03.py
@@ -0,0 +1,10 @@
+num1 = 20
+num2 = 5
+add = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+div = num1 / num2
+print("Add Result:", add)
+print("Sub Result:", sub)
+print("Multi Result:",multi)
+print("Div Result:",div)
