cfd43118449970d56e123814917a357fc0b3d6eb
gur02 <gur02bains@gmail.com>
2026-08-24 18:05:38 +0530

    Create day04.py

---
+++ day04.py
@@ -0,0 +1,8 @@
+first_name="Gurjit"
+last_name="kaur"
+full_name = first_name +" " + last_name
+print("Concatenation Result:", full_name)
+greeting_message = f"Hello , my name is Gurjit Kaur and I am on Day 4 of coding!"
+print("F-String Result:", greeting_message)
+print("Uppercase Version:",full_name.upper())
+print("Lowercase Version:",full_name.lower())
