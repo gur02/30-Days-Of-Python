1fbd1616c9c27cd1eaf9bb2265dce5f0fbb343fd
gur02 <gur02bains@gmail.com>
2026-08-26 17:15:44 +0530

    Create day05.py

---
+++ day05.py
@@ -0,0 +1,9 @@
+college_marks = 85
+if college_marks >=90:
+    print("Grade Result: Outstanding! You got an A+")
+elif college_marks >= 75:
+    print("Grade Result: First Class Division! You got an A")
+elif college_marks >=50:
+    print("Grade Result: Pass! You got a B+")
+else:
+    print("Grade Result: Fail! Need improvemrnt")
