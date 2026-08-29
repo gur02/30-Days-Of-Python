c2604a46aa924585e48871fbfa4de4bf724e6557
gur02 <gur02bains@gmail.com>
2026-08-29 18:52:35 +0530

    Create day09.py

---
+++ day09.py
@@ -0,0 +1,8 @@
+tools = ["Python", "VS code", "Terminal" ,"Git"]
+print("---Reading Specific Items---")
+print(f"My prime tool is: {tools[0]}")
+print(f"My code editor is: {tools[1]}")
+print("\n---Looping Through the Whole List---")
+for item in tools:
+    print(f"Automatic with: {item}")
+    
